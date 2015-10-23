1646c48977a8b03d425012c7bec0b7d8ab5ada09
Makc Klimm <makc.klimm@yandex.ru>
2015-10-23 14:59:13 +0300

    First

---
+++ config.rb
@@ -0,0 +1,17 @@
+require 'compass'
+
+http_path = "/app"
+css_dir = "app/css"
+sass_dir = "app/styles"
+images_dir = "app/images"
+javascripts_dir = "app/js"
+
+# output_style = :expanded or :nested or :compact or :compressed
+
+output_style = :expanded
+
+relative_assets = true
+
+line_comments = true
+
+preferred_syntax = :scss
